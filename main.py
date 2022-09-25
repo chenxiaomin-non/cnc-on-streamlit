25aa4350a9a55c7f5efcf0b370cd97011cbdbf5e
Truong Tran <truongtnnce150265@fpt.edu.vn>
2022-09-25 23:05:51 +0700

    init: simple cmc server

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import streamlit as st
+
+app = FastAPI()
+current_command = "no command"
+
+@app.get("/current_cmd")
+def get_current_command():
+    global current_command
+    return current_command
+
+
+def print_out_web_interface():
+    global current_command
+    st.header("Command Input")
+    st.text("\n\n\n")
+    
+    command = st.text_input("Input your command: ")
+
+    if st.button("Set Command"):
+        current_command = command
+        st.text("Command was set to: $ %s" % (command))
+
+if __name__ == "main":
+    print_out_web_interface()
+    
